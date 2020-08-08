9a4db82185e6197500708230ff347ccbbeb330e7
flamingcheeto <rorybraniff1@gmail.com>
2020-08-08 22:33:39 +1000

    Added number and backspace button to alter display

--- script.js
+++ script.js
@@ -17,3 +17,41 @@ function divide(x, y) {
 function operate(x, y, operator) {
     return operator(x, y);
 }
+
+function updateDisplay() {
+    let display = document.querySelector('#number-display');
+    display.textContent = getNumberStorage();
+
+}
+
+function setNumberStorage(update) {
+    if(getNumberStorage() == 0 && update == 0) {
+        return;
+    }
+    if(getNumberStorage().toString().length == 11 && !(update == getNumberStorage().toString().substr(0, getNumberStorage().toString().length - 1 ))) {
+        return;
+    }
+
+    sessionStorage.setItem('number', update);
+}
+
+function getNumberStorage() {
+    return sessionStorage.getItem('number');
+}
+
+let backButton = document.querySelector('#backspace');
+backButton.addEventListener('click', function() {
+    setNumberStorage( +getNumberStorage().toString().substr(0, getNumberStorage().toString().length - 1 ) );
+    updateDisplay();
+});
+
+let numericNodelist = document.querySelectorAll('.numeric-button');
+for (let i = 0; i < numericNodelist.length; i++) {
+    console.log(numericNodelist[i]);
+    let button = numericNodelist[i];
+    button.addEventListener('click', function() {
+        setNumberStorage( +(getNumberStorage().toString() + button.textContent) );
+        updateDisplay();
+    })
+}
+
